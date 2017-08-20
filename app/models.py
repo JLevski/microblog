fece31a209bcfaf6c1560a23b3b6daff3d7213ea
JLevski <jsalevin@gmail.com>
2017-08-20 17:34:40 +0200

    feature: initial commit

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from app import db
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), nullable=False, index=True, unique=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
